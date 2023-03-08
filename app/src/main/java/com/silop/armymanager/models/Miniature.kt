4dfd331f52eda8aa5644a8ce51387e196b09a34e
Silop024 <jackmdwebb@hotmail.com>
2023-03-08 11:51:58 +0100

    Changed miniature to entity

--- app/src/main/java/com/silop/armymanager/models/Miniature.kt
+++ app/src/main/java/com/silop/armymanager/models/Miniature.kt
@@ -1,10 +1,24 @@
 package com.silop.armymanager.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "minis")
 data class Miniature(
-    val name: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    var name: String,
+    @ColumnInfo(name = "base_points")
     var basePoints: Int = 0,
-    val equippedWeapons: MutableList<Weapon> = mutableListOf()
+    @ColumnInfo(name = "equipped_weapons")
+    val equippedWeapons: MutableList<Weapon> = mutableListOf(),
+    @ColumnInfo(name = "unit")
+    var unitName: String
 ) {
+    @get:Ignore
     val points: Int
         get() = equippedWeapons.sumOf { it.points } + basePoints
 }
